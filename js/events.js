ff83164c6ac2a7bd16331a03147f767107660f6d
asbjornholk <asbjornholk@gmail.com>
2023-05-16 16:54:32 +0200

    Fixed main page on mobile

--- js/events.js
+++ js/events.js
@@ -9,3 +9,18 @@ toggleEvent = function(button, event_id) {
 		event.style.maxHeight = event.scrollHeight + "px"
 	}
 }
+
+toggleTable = function(button, event_id, table_id) {
+	var event = document.getElementById(event_id)
+	var table = document.getElementById(table_id)
+
+	if (table.style.maxHeight) {
+		table.style.maxHeight = null
+		button.removeAttribute('open')
+		event.style.maxHeight = event.scrollHeight + "px"
+	} else {
+		button.setAttribute('open', true)
+		table.style.maxHeight = table.scrollHeight + "px"
+		event.style.maxHeight = event.scrollHeight + table.scrollHeight + "px"
+	}
+}
